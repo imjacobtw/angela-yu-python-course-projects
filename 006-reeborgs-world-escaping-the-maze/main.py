3a23859e98aaab99862e21157d9342f331b9ebf0
imjacobtw <imjacobtw@gmail.com>
2025-01-21 09:50:08 -0600

    Finish Day 6

---
+++ 006-reeborgs-world-escaping-the-maze/main.py
@@ -0,0 +1,17 @@
+def turn_right():
+    for _ in range(3):
+        turn_left()
+
+while front_is_clear():
+    move()
+    
+turn_left()
+       
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
