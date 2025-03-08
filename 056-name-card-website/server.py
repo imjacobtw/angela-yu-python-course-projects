1caa92cee9ff4c650defdcf89e606812638dadd7
imjacobtw <imjacobtw@gmail.com>
2025-03-07 20:48:47 -0600

    finish day 56: name card website

--- 056-name-card-website/server.py
+++ 056-name-card-website/server.py
@@ -2,11 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def home():
+def home_route() -> str:
     return render_template("index.html")
 
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
