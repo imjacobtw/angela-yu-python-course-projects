fb281e3781f238c6c9f4a60511a0ba4fca44aeaf
imjacobtw <imjacobtw@gmail.com>
2025-03-29 14:23:19 -0500

    finish day 68: flask auth

---
+++ 068-flask-auth/models/user.py
@@ -0,0 +1,11 @@
+from config.db import db
+from flask_login import UserMixin
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class User(UserMixin, db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(1000))
